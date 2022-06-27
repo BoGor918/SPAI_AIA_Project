411ee94259da9ee0e794cb1aa7ffdafeb00cb390
BoGor918 <72458648+BoGor918@users.noreply.github.com>
2022-06-27 11:25:23 +0800

    firebase

--- aia-event-project/src/firebase.js
+++ aia-event-project/src/firebase.js
@@ -1,19 +1,17 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
-import "firebase/auth"
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGS_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyClTTwSZmA0-Zf2_3ihpl4XEhosDciBrUM",
+  authDomain: "aia-project-auth-development.firebaseapp.com",
+  projectId: "aia-project-auth-development",
+  storageBucket: "aia-project-auth-development.appspot.com",
+  messagingSenderId: "559132296349",
+  appId: "1:559132296349:web:8a45da6f6a92ef9ca18074"
+};
 
 // Initialize Firebase
-export const auth = app.auth;
-export default app;
+const app = initializeApp(firebaseConfig);
